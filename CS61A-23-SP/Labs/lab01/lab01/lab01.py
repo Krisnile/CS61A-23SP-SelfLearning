6701dd116821f95dac29b5cdff9d08bdeedff3ba
Krisnile <139679983+Krisnile@users.noreply.github.com>
2023-08-21 08:33:52 +0800

    Add files via upload

---
+++ CS61A-23-SP/Labs/lab01/lab01/lab01.py
@@ -0,0 +1,93 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    if k==0:
+        return 1
+    else:
+        return n*falling(n-1,k-1)
+
+
+def divisible_by_k(n, k):
+    """
+    >>> a = divisible_by_k(10, 2)  # 2, 4, 6, 8, and 10 are divisible by 2
+    2
+    4
+    6
+    8
+    10
+    >>> a
+    5
+    >>> b = divisible_by_k(3, 1)  # 1, 2, and 3 are divisible by 1
+    1
+    2
+    3
+    >>> b
+    3
+    >>> c = divisible_by_k(6, 7)  # There are no integers up to 6 divisible by 7
+    >>> c
+    0
+    """
+    num=0
+    while (k*(num+1))<=n:
+        num+=1
+        print(k*num)
+    return num    
+        
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    if y<10:
+        return y
+    else:
+        rest,last=y//10,y%10
+        return sum_digits(rest)+last
+
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    flag=0
+    while n!=0:
+        if n%10==8:
+            flag+=1
+        else:
+            flag=0 
+        n//=10
+        if (flag==2)&(n%10!=8):
+            return True
+    return False
+
